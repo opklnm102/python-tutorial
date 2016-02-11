7121f31aedf49faae2aa2edcd37dfe9a20b1e739
DongHee Kim <opklnm102@gmail.com>
2016-02-11 11:08:09 +0900

    ex12.py 설명 추가

--- ex12.py
+++ ex12.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 
 #Dictionary
+#집합적 자료형
+#자료의 순서를 정하지 않는 Mapping형
+#	key를 이용하여 value에 접근
+#	시퀀스 자료형이 아님
+#key와 value의 mapping 1개를 item이라고 부름
+#value를 저장할 시에 key를 사용
+#	key가 없다면 새로운 key와 value의 아이템이 생성
+#	key가 이미 존재한다면 그 key에 해당하는 값이 변경
+#사전을 출력하면 각 아이템들이 임의의 순서로 출력
+#새로운 아이템이 들어오면 key에 따라 순서가 달라진다.
+#내부적으로 key 내용에 대해 Hash기법 사용
+#	검색속도가 매우 빠름
+#	[참고]: http://www.laurentluce.com/posts/python-dictionary-implementation/
+#key와 value 매핑에 대한 item을 삭제할 때는 del(key) 사용
+#key는 Immmutable. 문자열, 숫자, 튜플 가능
+#value는 임의의 객체
 print 'Dictionary'
 member = {'basketball': 5, 'soccer': 11, 'baseball': 9}
 print member['baseball']  #dictionary serarch
@@ -37,7 +53,7 @@ action2 = {add: 1, sub: 2}  #함수의 이름을 사전의 key로
 print action2[add]
 
 #dict()
-d = dict()
+d = dict()  #사전 생성  
 print type(d)
 print
 
@@ -77,6 +93,8 @@ print phone
 print ph  #ph는 바뀌지 않는다.
 
 #copy() -> shallow copy(o), deep copy(x)
+#shallow copy: 복사하려는 리스트 안 원소까지 복사X 
+#deep copy: 복사하려는 리스트 안 원소까지 복사
 phone = {'a': [1, 2, 3], 'b': 4}
 phone2 = phone.copy()
 print phone
@@ -105,11 +123,11 @@ print ph.get('gslee', 5284)  #get(key, default value)
 print ph
 print 
 
-print ph.popitem()
+print ph.popitem()  #임의의 아이템을 꺼낸다.
 print ph
 print
 
-print ph.pop('jack')
+print ph.pop('jack')  #key를 통해 해당 아이템을 지정하여 꺼낸다.
 print ph
 
 phone = {'jack': 946512, 'jin': 1111, 'joseph': 6584321}
@@ -123,11 +141,13 @@ print phone
 #loop
 print 'Loop'
 D = {'a': 1, 'b': 2, 'c':3}
+#사전의 모든 key를 순차적으로 참조
 for key in D.keys():  #key return
 	print key, D[key]
 
 print
 D = {'a': 1, 'b': 2, 'c':3}
+#사전 자체를 for에 활용하면 key에 대한 루프 실행
 for key in D:
 	print key, D[key]
 
@@ -138,9 +158,12 @@ for key, value in D.items():  #tuple로 된 list return
 
 print
 D = {'a': 1, 'b': 2, 'c': 3}
+#key, value 동시 참조
 for value in D.values():  #value가 list로 return
 	print value
 
+#사전에 입력된 아이템들은 일정한 순서X
+#key에 대한 정렬은 아이템들을 리스트로 뽑은 다음 해당 리스트에 있는 sort()활용
 print
 D = {'a': 1, 'b': 2, 'c': 3}
 items = D.items()  #tuple로 된 list return
